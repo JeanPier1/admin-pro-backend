c70d780bc82bde3dccda7c2692285d51c82e98f9
JeanPier1 <jeangonza@upeu.edu.pe>
2022-09-16 09:34:48 -0500

    config-usuario

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const express = require('express');
-require('dotenv').config();
-const cors = require('cors');
-const {dbConnection} = require('./database/config');
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const { dbConnection } = require("./database/config");
 
 // crear el servidor express
 const app = express();
 
-app.use( cors());
-
+app.use(cors());
 
 dbConnection();
 
-
-app.get('/',(req,res)=>{
-    res.json({
-        ok:true,
-        msg:"Hello Mundo"
-    })
-})
-
-app.listen(3000, () => {
-    console.log('servidor corriendo ' + process.env.PORT)
+app.get("/api/usuarios", (req, res) => {
+  res.json({
+    ok: true,
+    usuarios: [
+      {
+        id: 123,
+        nombre: "Fernando",
+      },
+    ],
+  });
 });
 
-
-
+app.listen(3005, () => {
+  console.log("servidor corriendo" + process.env.PORT);
+});
