74b648254742a1a57ec737cff4af3c37a55219e7
JeanPier1 <jeangonza@upeu.edu.pe>
2022-09-21 14:43:48 -0500

    10 finish

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const { dbConnection } = require("./database/config");
+
+// crear el servidor express
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+dbConnection();
+
+// Rutas
+app.use("/api/usuarios", require("./routes/usuarios"));
+
+app.use("/api/login", require("./routes/auth"));
+
+app.listen(3005, () => {
+  console.log("servidor corriendo" + process.env.PORT);
+});
