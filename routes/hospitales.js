ba9836575f7e0047aebeee5ca6ef10146a3cd80f
JeanPier1 <jeangonza@upeu.edu.pe>
2022-09-22 14:45:58 -0500

    controller hopitales and medico

--- routes/hospitales.js
+++ routes/hospitales.js
@@ -4,22 +4,40 @@
 */
 
 const { Router } = require("express");
+const { check } = require("express-validator");
 const {
   getHospital,
   crearHospital,
   actualizarHospital,
   deleteHospital,
 } = require("../controllers/hospital");
+const { validarCampos } = require("../middlewares/validar-campos");
 const { validarJWT } = require("../middlewares/validar-jwt");
 
 const router = Router();
 
 router.get("/", validarJWT, getHospital);
 
-router.post("/", [], crearHospital);
+router.post(
+  "/",
+  [
+    validarJWT,
+    check("nombre", "El nombre del hospital es necesario").not().isEmpty(),
+    validarCampos,
+  ],
+  crearHospital
+);
 
-router.put("/:id", [], actualizarHospital);
+router.put(
+  "/:id",
+  [
+    validarJWT,
+    check("nombre", "El nombre del hospital es necesario").not().isEmpty(),
+    validarCampos,
+  ],
+  actualizarHospital
+);
 
-router.delete("/:id", [], deleteHospital);
+router.delete("/:id", [validarJWT], deleteHospital);
 
 module.exports = router;
--- routes/medicos.js
+++ routes/medicos.js
@@ -4,12 +4,14 @@
 */
 
 const { Router } = require("express");
+const { check } = require("express-validator");
 const {
   getMedico,
   crearMedico,
   actualizarMedico,
   deleteMedico,
 } = require("../controllers/medico");
+const { validarCampos } = require("../middlewares/validar-campos");
 
 const { validarJWT } = require("../middlewares/validar-jwt");
 
@@ -17,10 +19,20 @@ const router = Router();
 
 router.get("/", validarJWT, getMedico);
 
-router.post("/", [], crearMedico);
+router.post("/", [
+  validarJWT,
+  check("nombre","El nombre del medico es necesario"),
+  check("idhospital","El id del hospital es necesario"),
+  validarCampos
+], crearMedico);
 
-router.put("/:id", [], actualizarMedico);
+router.put("/:id", [
+validarJWT,
+  check("nombre","El nombre del medico es necesario"),
+  check("idhospital","El id del hospital es necesario"),
+  validarCampos
+], actualizarMedico);
 
-router.delete("/:id", [], deleteMedico);
+router.delete("/:id", [validarJWT], deleteMedico);
 
 module.exports = router;
